f23986ab3701f7faefbf071a31362d2dc114aa05
shimele <bechembayamba@gmail.com>
2020-10-31 17:13:15 +0100

    project page

---
+++ script.js
@@ -0,0 +1,14 @@
+//set a CSS custom property that knows the percentage of the page scrolled:
+//for CSS scroll animation
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
+//for AOS animation
+AOS.init();
